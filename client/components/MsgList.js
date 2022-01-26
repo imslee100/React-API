43ff3052029174f878762fa0544dfdc031f118fe
Steve Lee <imslee100@gmail.com>
2022-01-27 00:30:00 +0900

    Write Function

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import MsgItem from './MsgItem'
+import MsgInput from './MsgInput'
 
 const UserIds = ['roy','jay']
 const getRandomUserId = () => UserIds[Math.round(Math.random())]
 
-const msgs = Array(50)
-    .fill(0).
-    map((_, i) => ({
+const originalMsgs = Array(50)
+    .fill(0)
+    .map((_, i) => ({
     id: 50 - i,
     userId: getRandomUserId(),
     timestamp: 1234567890123 + (50-i) * 1000 * 60,
@@ -23,12 +25,29 @@ const msgs = Array(50)
 ]
 */
 
-const MsgList = () => (
-    <ul className="messages">{
-        msgs.map(x => (
-        <MsgItem key={x.id} { ... x} />
+const MsgList = () => {
+    const [msgs, setMsgs] = useState(originalMsgs)
+    const onCreate = text => {
+        const newMsg = {
+            id: msgs.length + 1,
+            userId: getRandomUserId(),
+            timestamp: Date.now(),
+            text: `${msgs.length+1} ${text}`,
+        }
+        setMsgs(msgs => ([newMsg, ... msgs]))
+        console.log(msgs)
+    }
+
+    return (
+    <>
+        <MsgInput mutate={onCreate} />
+        <ul className="messages">
+        {msgs.map(x => (
+            <MsgItem key={x.id} { ... x} />
         ))}
-    </ul>
-)
+        </ul>
+    </>
+    )
+}
 
 export default MsgList
