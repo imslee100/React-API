1ebb4cfaf0e21a155428e3cff0ad07b280d685b6
Steve Lee <imslee100@gmail.com>
2022-01-26 22:23:07 +0900

    List View

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,10 @@
-const Home = () => {
+import MsgList from '../components/MsgList'
+
+const Home = () => (
     <>
         <h1>SIMPLE SNS</h1>
+        <MsgList />
     </>
-}
+)
 
 export default Home
